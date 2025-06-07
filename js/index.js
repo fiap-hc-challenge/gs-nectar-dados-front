bf9bb8eddcb86fccc627a6efab37b40000283515
grazialencar <graziellealencar3@gmail.com>
2025-06-07 16:34:51 -0300

    Colocar cor para o header quando passa de 200px da pág index.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Abrir e fechar menu mobile
 let btnMenu = document.getElementById('btn-menu')
 let menuMobile = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -13,3 +14,15 @@ menuMobile.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menuMobile.classList.remove('abir-menu')
 })
+
+
+//header subindo
+document.addEventListener("scroll", function() {
+    const header = document.getElementById("header");
+
+    if (window.scrollY > 200) {
+        header.style.backgroundColor = "#003B30";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
